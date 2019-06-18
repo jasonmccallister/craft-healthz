bed232178712cf4969250adf8e1d6de3302120d5
Jason McCallister <themccallister@gmail.com>
2019-06-18 03:12:26 -0400

    init

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Health Check plugin for Craft CMS 3.x
+ *
+ * Add health checks to your Craft website, useful when running inside of Docker
+ *
+ * @link      https://mccallister.io
+ * @copyright Copyright (c) 2019 Jason McCallister
+ */
+
+namespace jasonmccallister\healthcheck\controllers;
+
+use jasonmccallister\healthcheck\HealthCheck;
+
+use Craft;
+use craft\web\Controller;
+
+/**
+ * Default Controller
+ *
+ * Generally speaking, controllers are the middlemen between the front end of
+ * the CP/website and your plugin’s services. They contain action methods which
+ * handle individual tasks.
+ *
+ * A common pattern used throughout Craft involves a controller action gathering
+ * post data, saving it on a model, passing the model off to a service, and then
+ * responding to the request appropriately depending on the service method’s response.
+ *
+ * Action methods begin with the prefix “action”, followed by a description of what
+ * the method does (for example, actionSaveIngredient()).
+ *
+ * https://craftcms.com/docs/plugins/controllers
+ *
+ * @author    Jason McCallister
+ * @package   HealthCheck
+ * @since     1.0.0
+ */
+class DefaultController extends Controller
+{
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     *         The actions must be in 'kebab-case'
+     * @access protected
+     */
+    protected $allowAnonymous = ['index'];
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Handle a request going to our plugin's index action URL,
+     * e.g.: actions/health-check/default
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $result = 'Welcome to the DefaultController actionIndex() method';
+
+        return $result;
+    }
+
+    /**
+     * Handle a request going to our plugin's actionDoSomething URL,
+     * e.g.: actions/health-check/default/do-something
+     *
+     * @return mixed
+     */
+    public function actionDoSomething()
+    {
+        $result = 'Welcome to the DefaultController actionDoSomething() method';
+
+        return $result;
+    }
+}
